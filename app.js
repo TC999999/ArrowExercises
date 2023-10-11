0719ce0981e6a543cbdebc76ab92d23a50556561
Tyler <insertcleverusernamehere999@gmail.com>
2023-10-11 13:14:08 -0400

    added exercise files

---
+++ app.js
@@ -0,0 +1,29 @@
+//Exercise 1:
+//ES5
+// function double(arr) {
+//   return arr.map(function (val) {
+//     return val * 2;
+//   });
+// }
+
+//ES2015
+const double = (arr) => arr.map((val) => val * 2);
+
+//Exercise 2
+//ES5
+// function squareAndFindEvens(numbers) {
+//   var squares = numbers.map(function (num) {
+//     return num ** 2;
+//   });
+//   var evens = squares.filter(function (square) {
+//     return square % 2 === 0;
+//   });
+//   return evens;
+// }
+
+//ES2015
+const squareAndFindEvens = (numbers) => {
+  var squares = numbers.map((num) => num ** 2);
+  var evens = squares.filter((square) => square % 2 === 0);
+  return evens;
+};
